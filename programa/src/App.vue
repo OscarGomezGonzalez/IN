b516fccc6c6cf607c2822a71e6f9956c699f253d
Oscar <oscar.gomezgonzalez95@gmail.com>
2020-05-19 07:46:26 +0200

    nuevo input para cambiar de tema

--- programa/src/App.vue
+++ programa/src/App.vue
@@ -1,53 +1,70 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn-prod.scalefast.com/public/assets/img/resized/bandai-namco-store-eu/7963e1ccb39f44bbb6c2e490775a487e_400_KR.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar
+                app
+                color="primary"
+                dark
+        >
+            <div class="d-flex align-center">
+                <v-img
+                        alt="Vuetify Logo"
+                        class="shrink mr-2"
+                        contain
+                        src="https://cdn-prod.scalefast.com/public/assets/img/resized/bandai-namco-store-eu/7963e1ccb39f44bbb6c2e490775a487e_400_KR.png"
+                        transition="scale-transition"
+                        width="40"
+                />
 
-        <h1>Solaire</h1>
-      </div>
+                <h1>Solaire</h1>
+            </div>
 
-      <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <!-- theme switcher-->
+            <div>
+                <input type="checkbox" id='theme-switch' class='theme-switch' v-model="$vuetify.theme.dark"/>
+                <label for='theme-switch'>
+                         <span v-if="$vuetify.theme.dark === true">
+                             <img alt="logo" src="./assets/images/moon.png" width="40">
+                         </span>
+                    <span v-else>
+                             <img alt="logo" src="./assets/images/sun.png" width="40">
+                         </span>
+                </label>
+            </div>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+            <v-spacer></v-spacer>
+
+            <v-btn
+                    href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                    target="_blank"
+                    text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-content>
+            <HelloWorld/>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+    import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+        components: {
+            HelloWorld,
+        },
 
-  data: () => ({
-    //
-  }),
-};
+        data() {
+            return {
+                darkMode: false,
+            }
+        },
+    };
 </script>
