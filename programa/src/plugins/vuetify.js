d10964cf448ea1a0a28cd548dfa8e65c296675da
carlosmz96 <carlosmz96esp@gmail.com>
2020-05-19 13:58:19 +0200

    Cambio vuetify.js

--- programa/src/plugins/vuetify.js
+++ programa/src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+
+    theme: {
+        themes: {
+            light: {
+                primary: '#323232',
+                secondary: '#b0bec5',
+                anchor: '#8c9eff',
+            },
+            dark: {
+                primary: '#323232',
+                secondary: '#b0bec5',
+                anchor: '#8c9eff',
+            }
+        },
+    },
+
 });
