b225f06088d3ccc89973035de76e25f17603cad9
Oscar <oscar.gomezgonzalez95@gmail.com>
2020-05-19 08:13:51 +0200

    Se me paso la coma

--- programa/src/App.vue
+++ programa/src/App.vue
@@ -60,7 +60,7 @@
             {icon: "home", title: "Link A"},
             {icon: "info", title: "Link B"},
             {icon: "warning", title: "Link C"}
-        ]
+        ],
 
         components: {
             HelloWorld,
